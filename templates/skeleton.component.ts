ba4bdef0eeb897d24dc31f29ebbf1a945e428b3d
Jonathan <jonathan.chen@sunrider.com>
2020-03-04 10:15:06 -0700

    made more skeleton stuff.

---
+++ templates/skeleton.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: 'app.component.ts',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(){}
+}
+
+
+
